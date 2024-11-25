d71adc9ed2222d34e7ca6266b756055c9d9b3369
brittLiban <britt.liban@student.greenriver.edu>
2024-11-24 23:32:16 -0800

    First Commmit

---
+++ app.js
@@ -0,0 +1,51 @@
+// Get the express package 
+const express = require('express');
+const mariadb = require('mariadb')
+const nodemon = require('nodemon')
+// Instantiate an express (web) app
+const app = express();
+
+// Define a port number for 
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+// configure the connection 
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Black202',
+    database: 'guestbook'
+});
+
+// Connect to the database
+async function connect() {
+    try {
+        let conn = await pool.getConnection();
+        console.log('Connected to the database');
+        return conn;
+    } catch (err) {
+        console.log('Error connecting to the database: ' + err);
+    }
+}
+
+// Tell the app to encode data into JSON format
+app.use(express.urlencoded({ extended: false }));
+
+// Set your view (templating) engine to "EJS"
+// (We use a templating engine to create dynamic web pages)
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    // Log message to the server's console
+    console.log("Hello, world - server!");
+    res.render('home');
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
